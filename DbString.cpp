2d1ef1601bb1bcef91fe6c2e5ce1dca7f876a04a
animatedb <animatedb@users.noreply.github.com>
2016-11-05 08:29:52 -0700

    Add files via upload

---
+++ DbString.cpp
@@ -0,0 +1,187 @@
+/*
+* DbString.cpp
+*
+*  Created on: Nov 2, 2016
+*  \copyright 2016 DCBlaha.  Distributed under the GPL.
+*/
+
+#include "DbString.h"
+#include <ctype.h>
+
+
+// Finds comma delimited arguments. Spaces are skipped.
+static size_t findNextArg(StringRef args, size_t &startArgPos)
+    {
+    size_t retArgLen = 0;
+    if(startArgPos != String::npos)
+        {
+        while(args[startArgPos] == ' ')
+            { startArgPos++; }
+        size_t endArgPos = args.find(',', startArgPos);
+        if(endArgPos != std::string::npos)
+            {
+            endArgPos++;        // Skip comma
+            retArgLen = endArgPos - startArgPos;
+            }
+        else
+            {
+            retArgLen = args.length()-startArgPos;
+            }
+        }
+    return retArgLen;
+    }
+
+// Appends comma delimited arguments to a string.
+static void appendArg(String &targetString, StringRef args, size_t startArgPos)
+    {
+    int len = findNextArg(args, startArgPos);
+    targetString.append(&args[startArgPos], len);
+    if(startArgPos != String::npos)
+        {
+        targetString.append(",");
+        }
+    }
+
+static inline void appendColumnName(String &targetString, StringRef columns, size_t startColumnPos)
+    {
+    appendArg(targetString, columns, startColumnPos);
+    }
+
+static inline void appendValue(String &targetString, StringRef values, size_t startValuePos)
+    {
+    targetString.append("\"");
+    appendArg(targetString, values, startValuePos);
+    targetString.append("\"");
+    }
+
+static void appendBoundValues(String &targetString, size_t numBoundValues)
+    {
+    for(int i=0; i<numBoundValues; i++)
+        {
+        if(i != 0)
+            { targetString.append(","); }
+        targetString.append("?");
+        }
+    }
+
+static void appendColumnNames(String &targetString, StringRef colNames)
+    {
+    size_t startArgPos = 0;
+    while(startArgPos != String::npos)
+        {
+        appendColumnName(targetString, colNames, startArgPos);
+        }
+    }
+
+// Appends values with no outer parenthesis
+static void appendValues(String &targetString, StringRef values)
+    {
+    size_t startArgPos = 0;
+//    if(values.length() != 0)
+        {
+        // parse string and separate into values.
+        while(startArgPos != String::npos)
+            {
+            appendValue(targetString,  values, startArgPos);
+            }
+        }
+// Should use bind null
+//    else
+//        {
+//        append("NULL");
+//        }
+    }
+
+static void appendNamesAndValues(String &targetString, StringRef columnNames,
+    StringRef values)
+    {
+    size_t startColPos = 0;
+    size_t startValuePos = 0;
+    while(startColPos != String::npos && startValuePos != String::npos)
+        {
+        appendColumnName(targetString, columnNames, startColPos);
+        targetString.append("=");
+        appendValue(targetString, values, startValuePos);
+        if(startColPos != String::npos && startValuePos != String::npos)
+            {
+            targetString.append(",");
+            }
+        }
+    }
+
+void DbString::startStatement(StringRef statement, StringRef arg)
+    {
+    clear();
+    append(statement);
+    append(arg);
+    }
+
+DbString &DbString::FROM(StringRef table)
+    {
+    append(" FROM ");
+    append(table);
+    return *this;
+    }
+
+DbString &DbString::SET(StringRef columns, size_t numBoundValues)
+    {
+    append(" SET ");
+//    appendNamesAndValues(*this, columns, numBoundValues);
+    return *this;
+    }
+
+DbString &DbString::WHERE(StringRef columnName, StringRef operStr, size_t numBoundValues)
+    {
+    append(" WHERE ");
+    append(columnName);
+    append(operStr);
+    append("(");
+    appendBoundValues(*this, numBoundValues);
+    append(")");
+    return *this;
+    }
+
+DbString &DbString::AND(StringRef columnName, StringRef operStr, size_t numBoundValues)
+    {
+    append(" AND ");
+    append(columnName);
+    append(operStr);
+    append("(");
+    appendBoundValues(*this, numBoundValues);
+    append(")");
+    return *this;
+    }
+
+DbString &DbString::INTO(StringRef columnNames)
+    {
+    append("(");
+    appendColumnNames(*this, columnNames);
+    append(")");
+    return *this;
+    }
+
+DbString &DbString::VALUES(StringRef columnValues)
+    {
+    append("VALUES (");
+    appendValues(*this, columnValues);
+    append(")");
+    return *this;
+    }
+
+DbString &DbString::VALUES(size_t numBoundValues)
+    {
+    append("VALUES (");
+    appendBoundValues(*this, numBoundValues);
+    append(")");
+    return *this;
+    }
+
+String DbString::getDbStr()
+    {
+    size_t len = length();
+    if(len > 0 && (*this)[len-1] != ';')
+        {
+        append(";");
+        }
+    return *this;
+    }
---
+++ DbString.h
@@ -0,0 +1,85 @@
+/*
+* DbString.h
+*
+*  Created on: Nov 2, 2016
+*  \copyright 2016 DCBlaha.  Distributed under the GPL.
+*/
+
+#ifndef DB_STRING_H
+#define DB_STRING_H
+
+#include <string>
+#include <vector>
+
+typedef std::string const &StringRef;
+typedef std::string String;
+
+// This uses method chaining or the named parameter idiom.
+// Examples:
+//      dbStr.SELECT("idModule").FROM("Module").WHERE("name", "=").VALUES(2);
+//      dbStr.INSERT("Module").INTO("idModule, name").VALUES(2);
+//      dbStr.UPDATE("Module").SET("idModule, name").VALUES(2);
+//      dbStr.DELETE("idModule").FROM("Module");
+class DbString:public String
+    {
+    public:
+        DbString()
+            {}
+        DbString(StringRef str):
+            String(str)
+            {}
+        // Appends "SELECT column"
+        DbString &SELECT(StringRef column)
+            {
+            startStatement("SELECT ", column);
+            return *this;
+            }
+
+        // Appends "INSERT INTO table"
+        DbString &INSERT(StringRef table)
+            {
+            startStatement("INSERT INTO ", table);
+            return *this;
+            }
+
+        // Appends "UPDATE table"
+        DbString &UPDATE(StringRef table)
+            {
+            startStatement("UPDATE ", table);
+            return *this;
+            }
+
+        DbString &DELETE(StringRef table)
+            {
+            startStatement("DELETE FROM ", table);
+            return *this;
+            }
+
+        // Appends " FROM "
+        DbString &FROM(StringRef table);
+        // Appends "(columnName1, columnName2)"
+        DbString &INTO(StringRef columnNames);
+        // Appends "VALUES (columnValue1, columnValue2)"
+        DbString &VALUES(StringRef columnValues);
+        DbString &VALUES(size_t numBoundValues);
+
+        // Appends " SET column1, column2 = value1, value2"
+        // The number of columns and values must match.
+        DbString &SET(StringRef columnNames, size_t numBoundValues);
+
+        // Appends " WHERE columnName operStr colVal"
+        DbString &WHERE(StringRef columnName, StringRef operStr,
+            size_t numBoundValues);
+        // Appends " AND columnName operStr colVal"
+        DbString &AND(StringRef columnName, StringRef operStr,
+            size_t numBoundValues);
+        // Appends a semicolon to the returned string.
+        String getDbStr();
+
+    private:
+        // Prevent usage. Use getDbStr instead. This is undefined.
+        char const *c_str();
+        void startStatement(StringRef statement, StringRef arg);
+    };
+
+#endif
